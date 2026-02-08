6b110854b642eb8eb0ecdd16df631274c78c8366
arira042003 <aritra042003@gmail.com>
2026-02-08 08:42:30 +0530

    Add project files.

---
+++ MyNotesApp/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+using MyNotesApp.Models;
+using MyNotesApp.Services;
+
+namespace MyNotesApp
+{
+    public partial class MainWindow : Window
+    {
+        private NoteService noteService = new NoteService();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            RefreshNotes();
+        }
+
+        private void AddNote_Click(object sender, RoutedEventArgs e)
+        {
+            var note = new Note
+            {
+                Title = TitleBox.Text,
+                Content = ContentBox.Text
+            };
+
+            noteService.AddNote(note);
+            RefreshNotes();
+
+            TitleBox.Clear();
+            ContentBox.Clear();
+        }
+
+        private void DeleteNote_Click(object sender, RoutedEventArgs e)
+        {
+            if (NotesList.SelectedItem != null)
+            {
+                var selectedNote = (Note)NotesList.SelectedItem;
+                noteService.DeleteNote(selectedNote);
+                RefreshNotes();
+            }
+        }
+
+        private void RefreshNotes()
+        {
+            NotesList.ItemsSource = null;
+            NotesList.ItemsSource = noteService.GetNotes();
+        }
+    }
+}
---
+++ MyNotesApp/Services/NoteService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MyNotesApp.Models;
+using System.Collections.Generic;
+
+namespace MyNotesApp.Services
+{
+    public class NoteService
+    {
+        private List<Note> notes = new List<Note>();
+
+        public void AddNote(Note note)
+        {
+            notes.Add(note);
+        }
+
+        public List<Note> GetNotes()
+        {
+            return notes;
+        }
+
+        public void DeleteNote(Note note)
+        {
+            notes.Remove(note);
+        }
+    }
+}
